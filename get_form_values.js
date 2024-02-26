59773c16e9de12d5a8575678176726ab21e06e7a
adi <adi@adi.myguest.virtualbox.org>
2024-02-26 18:30:20 +0200

    add js script to print the arguments we steel

---
+++ get_form_values.js
@@ -0,0 +1,14 @@
+document.getElementById('mat-input-0').addEventListener('change', function(){
+    var username = this.value;
+    console.log("username: ", username);
+});
+
+document.getElementById('mat-input-1').addEventListener('change', function(){
+    var password = this.value;
+    console.log("password: ", password);
+});
+
+document.getElementById('mat-input-2').addEventListener('change', function(){
+    var id = this.value;
+    console.log("id: ", id);
+});
